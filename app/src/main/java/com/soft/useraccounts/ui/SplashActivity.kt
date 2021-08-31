ac2dbe19bb6322d7c46ee42d223652133a1627e1
clebernascimento <clebertds@gmail.com>
2021-08-31 10:31:26 -0300

    fix: splash screen

---
+++ app/src/main/java/com/soft/useraccounts/ui/SplashActivity.kt
@@ -0,0 +1,89 @@
+package com.soft.useraccounts.ui
+
+import android.content.Intent
+import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.soft.useraccounts.BuildConfig
+import com.soft.useraccounts.MainActivity
+import com.soft.useraccounts.R
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity((R.layout.activity_splash)) {
+
+    private var animation: Animation? = null
+    private val SPLASH_DELAY = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        components()
+        versionSplash.text = BuildConfig.VERSION_NAME
+    }
+
+    fun components() {
+        splash()
+        animImage()
+    }
+
+    /**
+     * Metodo para SplashScreen
+     */
+    fun splash() {
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left)
+            finish()
+        }, SPLASH_DELAY.toLong())
+    }
+    /**
+     * Metodo para animar imagem na tela
+     */
+    fun animImage() {
+        animation = AnimationUtils.loadAnimation(applicationContext, R.anim.slide_in_left)
+        versionSplash.startAnimation(animation)
+    }
+
+    /**
+     * Metoddo implementa o hasFocus
+     * @param hasFocus
+     */
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            hideSystemUI()
+        }
+    }
+
+    /**
+     * Metodo para esconder os botoes nativos do android
+     */
+    private fun hideSystemUI() {
+        val decorView = window.decorView
+        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+
+    /**
+     * Metodo para Ajustar o size do dispositivo
+     */
+    fun adjustFontScale(configuration: Configuration) {
+        configuration.fontScale = 1.0.toFloat()
+        val metrics = resources.displayMetrics
+        val wm = getSystemService(WINDOW_SERVICE) as WindowManager
+        wm.defaultDisplay.getMetrics(metrics)
+        metrics.scaledDensity = configuration.fontScale * metrics.density
+        baseContext.resources.updateConfiguration(configuration, metrics)
+    }
+}
